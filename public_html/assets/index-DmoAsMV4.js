900fb7bf13616ff6e59a4464f0c316904de10bcc
Avis Ejerenwa <ejerenwaavis.com>
2026-09-15 07:07:55 -0400

    ci: allow release publish on pin-fix and rebuild client assets

--- public_html/assets/index-DmoAsMV4.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CJ79HYGQ.js","./index-oBB6vBcd.js","./index-Dpm6FsTB.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-oBB6vBcd.js";const _=e("Browser",{web:()=>o(()=>import("./web-CJ79HYGQ.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.BrowserWeb)});export{_ as Browser};
